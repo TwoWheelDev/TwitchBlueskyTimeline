3b673aec143c4da327fdfbc457940204851f8ac3
Daniel Lintott <daniel.j.lintott@gmail.com>
2025-05-11 01:43:06 +0100

    🚧 build(app): Add manual chunking to improve chunk sizes

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,25 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
   server: {
     allowedHosts: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "vendor-react";
+            if (id.includes("react-router")) return "vendor-router";
+            if (id.includes("lucide-react")) return "vendor-icons";
+            if (id.includes("@atproto")) return "vendor-atproto";
+            if (id.includes("@icons-pack")) return "vendor-simple-icons";
+            return "vendor"; // fallback for other node_modules
+          }
+        }
+      }
+    }
   }
 })
