a231ca1ca697cce93890ec899019bc43640d05c6
Daniel Lintott <daniel.j.lintott@gmail.com>
2025-05-11 20:20:22 +0100

    ✨ feat(app): Switch to using HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import Panel from './Panel.tsx'
 import Config from './Config.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter basename='/'>
     <Routes>
-      <Route path='/panel' element={<Panel />} />
-      <Route path='/config' element={<Config />} />
+      <Route path='panel' element={<Panel />} />
+      <Route path='config' element={<Config />} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
